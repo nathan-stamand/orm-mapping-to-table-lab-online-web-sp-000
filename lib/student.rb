25fd34e3466f09c24d2b493feeea8339c10fa012
Nathan St.Amand <nathansaint810@gmail.com>
2020-03-26 19:39:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,7 +42,7 @@ class Student
     
     DB[:conn].execute(sql, self.name, self.grade)
     
-    @id = DB[:conn].execute("SELECT last_insert_rowid()")
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")
     
   end
   
